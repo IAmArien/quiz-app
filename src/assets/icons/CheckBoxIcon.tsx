3ab0a52b556dc1cec7d94574ebb4f77b70d7d959
Norman Palisoc <palisocnorman@gmail.com>
2024-05-25 10:27:49 +0800

    feat: add update profile

---
+++ src/assets/icons/CheckBoxIcon.tsx
@@ -0,0 +1,56 @@
+/**
+ * Property of the Earn G. Bautista and Team
+ * Reuse as a whole or in part is prohibited without permission.
+ */
+
+import * as React from "react";
+
+type TCheckBoxIconProps = {
+  checked?: boolean;
+};
+
+export const CheckBoxIcon: React.FC<TCheckBoxIconProps> = ({
+  checked = true,
+}): JSX.Element => {
+  if (checked) {
+    return (
+      <svg
+        data-testid="checkbox-icon-checked"
+        width="20"
+        height="20"
+        viewBox="0 0 20 20"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <rect width="20" height="20" rx="4" fill="#001A88" />
+        <path
+          d="M6 11L8.5 13.5L15 7"
+          stroke="white"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+      </svg>
+    );
+  }
+  return (
+    <svg
+      data-testid="checkbox-icon-unchecked"
+      width="20"
+      height="20"
+      viewBox="0 0 20 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect
+        x="0.5"
+        y="0.5"
+        width="19"
+        height="19"
+        rx="3.5"
+        fill="white"
+        stroke="#D7D7D7"
+      />
+    </svg>
+  );
+};
--- src/screens/instructor/Profile/index.tsx
+++ src/screens/instructor/Profile/index.tsx
@@ -3,12 +3,27 @@
  * Reuse as a whole or in part is prohibited without permission.
  */
 
-import { Container } from "react-bootstrap";
+import { Button, Col, Container, Form, Row } from "react-bootstrap";
 import { MainContainer } from "../../../components";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { CheckBoxIcon } from "../../../assets/icons/CheckBoxIcon";
 
 export const Profile = (): JSX.Element => {
   const navigate = useNavigate();
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [department, setDepartment] = useState("");
+  const [emailAddress, setEmailAddress] = useState("");
+  const [updatePassword, setUpdatePassword] = useState(false);
+  const [password, setPassword] = useState("");
+  const [newPasword, setNewPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const handleOnSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  };
+
   return (
     <MainContainer
       title="Profile"
@@ -58,7 +73,117 @@ export const Profile = (): JSX.Element => {
         }
       ]}>
       <Container>
-        <div></div>
+        <div>
+          <Row>
+            <Col lg={6} md={8} sm={12}>
+              <div className="bg-[#FFFFFF] rounded-[10px] pt-[30px]">
+                <h3 className="text-[25px] open-sans">
+                  Update your Instructor account
+                </h3>
+                <p className="text-[#8c8c8c] text-[15px] open-sans mt-[8px]">
+                  Please fill up all the required fields to update your account.
+                </p>
+                <Form onSubmit={handleOnSubmit} className="mt-[30px]">
+                  <div className="flex flex-col gap-[10px]">
+                    <div className="flex flex-row gap-[10px]">
+                      <input
+                        type="text"
+                        name="firstname"
+                        className="form-control open-sans"
+                        placeholder="First Name"
+                        required
+                        value={firstName}
+                        onChange={(event) => {
+                          setFirstName(event.currentTarget.value);
+                        }}
+                      />
+                      <input
+                        type="text"
+                        name="lastname"
+                        className="form-control open-sans"
+                        placeholder="Last Name"
+                        required
+                        value={lastName}
+                        onChange={(event) => {
+                          setLastName(event.currentTarget.value);
+                        }}
+                      />
+                    </div>
+                    <input
+                      type="text"
+                      name="department"
+                      className="form-control open-sans"
+                      placeholder="Department / College"
+                      required
+                      value={department}
+                      onChange={(event) => {
+                        setDepartment(event.currentTarget.value);
+                      }}
+                    />
+                    <input
+                      type="email"
+                      name="email"
+                      className="form-control open-sans"
+                      placeholder="Email Address"
+                      required
+                      disabled
+                      value={emailAddress}
+                      onChange={(event) => {
+                        setEmailAddress(event.currentTarget.value);
+                      }}
+                    />
+                    <div className="flex flex-row gap-[10px] items-center mt-[20px] mb-[6px] cursor-pointer"
+                      onClick={() => setUpdatePassword(!updatePassword)}>
+                      <CheckBoxIcon checked={updatePassword} />
+                      <h3 className="open-sans-600 text-[15px]">
+                        Update Password?
+                      </h3>
+                    </div>
+                    <input
+                      type="password"
+                      name="current_password"
+                      className="form-control open-sans"
+                      placeholder="Current Password"
+                      required
+                      disabled={!updatePassword}
+                      value={password}
+                      onChange={(event) => {
+                        setPassword(event.currentTarget.value);
+                      }}
+                    />
+                    <input
+                      type="password"
+                      name="new_password"
+                      className="form-control open-sans"
+                      placeholder="New Password"
+                      required
+                      disabled={!updatePassword}
+                      value={newPasword}
+                      onChange={(event) => {
+                        setNewPassword(event.currentTarget.value);
+                      }}
+                    />
+                    <input
+                      type="password"
+                      name="password"
+                      className="form-control open-sans"
+                      placeholder="Confirm Password"
+                      required
+                      disabled={!updatePassword}
+                      value={confirmPassword}
+                      onChange={(event) => {
+                        setConfirmPassword(event.currentTarget.value);
+                      }}
+                    />
+                    <Button className="open-sans-600" type="submit" variant="success">
+                      Update Profile
+                    </Button>
+                  </div>
+                </Form>
+              </div>
+            </Col>
+          </Row>
+        </div>
       </Container>
     </MainContainer>
   );
