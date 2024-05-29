8d3b48fdf876c129d639b896828519d7a08df4f5
Norman Palisoc <palisocnorman@gmail.com>
2024-05-29 23:18:42 +0800

    feat: initial display of subjects

--- src/screens/students/Subjects/index.tsx
+++ src/screens/students/Subjects/index.tsx
@@ -5,11 +5,107 @@
 
 import { useNavigate } from "react-router-dom";
 import { MainContainer } from "../../../components";
+import { Badge, Button } from "react-bootstrap";
+import { useEffect, useState } from "react";
+import DataTable from "react-data-table-component";
+import { CSSObject } from "styled-components";
+
+type TDataTableSubjectsData = {
+  id: number;
+  subjectId: number;
+  subject: string;
+  section: string;
+  description: string;
+  assessments: string;
+};
 
 export const Subjects = (): JSX.Element => {
   const navigate = useNavigate();
+  const [selectedSubject, setSelectedSubject] = useState<TDataTableSubjectsData | null>(null);
+  const [data, setData] = useState<TDataTableSubjectsData[]>([]);
   const firstName = sessionStorage.getItem("student.firstname");
   const lastName = sessionStorage.getItem("student.lastname");
+
+  const columns = [
+    {
+      name: 'Subject (Title)',
+      selector: (row: TDataTableSubjectsData) => row.subject,
+      sortable: true,
+      cell: (row: TDataTableSubjectsData) => (
+        <p className="open-sans-600 text-[15px]">
+          {row.subject}
+        </p>
+      )
+    },
+    {
+      name: 'Section',
+      selector: (row: TDataTableSubjectsData) => row.section,
+      sortable: true,
+      cell: (row: TDataTableSubjectsData) => (
+        <p className="open-sans-600 text-[15px]">
+          {row.section}
+        </p>
+      )
+    },
+    {
+      name: 'Subject (Description)',
+      selector: (row: TDataTableSubjectsData) => row.description,
+      sortable: true,
+      cell: (row: any) => (
+        <div className="flex flex-row gap-[12px] items-center pr-[50px]">
+          <p className="open-sans text-[#8c8c8c] text-[15px]">
+            {row.description}
+          </p>
+        </div>
+      )
+    },
+    {
+      name: 'Active Assessments',
+      selector: (row: TDataTableSubjectsData) => row.assessments,
+      sortable: true,
+      cell: (row: any) => (
+        <div className="flex flex-row gap-[12px] items-center pr-[50px]">
+          <Badge bg="success" className="open-sans-700 uppercase">
+            {row.assessments}
+          </Badge>
+        </div>
+      )
+    },
+    {
+      name: 'Actions',
+      sortable: false,
+      cell: (row: TDataTableSubjectsData) => (
+        <div className="flex flex-row gap-[5px]">
+          <Button variant="outline-success" size="sm" onClick={() => {
+            setSelectedSubject(row);
+          }}>
+            <i className="fa-solid fa-eye"></i>&nbsp;&nbsp;View Assessments
+          </Button>
+        </div>
+      )
+    }
+  ];
+
+  useEffect(() => {
+    setData([
+      {
+        id: 1,
+        subjectId: 1,
+        subject: "Mathematics II",
+        section: "Section I",
+        description: "Math subject with 3 sections",
+        assessments: "0 Assessments"
+      }
+    ])
+  }, []);
+
+  const headCellStyle: CSSObject = {
+    fontSize: 15,
+    fontFamily: '"Open Sans", sans-serif',
+    fontOpticalSizing: "auto",
+    fontWeight: 700,
+    fontStyle: "normal",
+  };
 
   return (
     <MainContainer
@@ -55,7 +151,24 @@ export const Subjects = (): JSX.Element => {
           }
         },
       ]}>
-      <div></div>
+      <div className="flex flex-col pt-[20px]">
+        <DataTable
+          title="Assigned Subjects"
+          striped
+          selectableRows={false}
+          dense={false}
+          highlightOnHover
+          pagination
+          className="open-sans-600"
+          customStyles={{
+            headCells: {
+              style: headCellStyle
+            }
+          }}
+          columns={columns}
+          data={data}
+        />
+      </div>
     </MainContainer>
   );
 };
